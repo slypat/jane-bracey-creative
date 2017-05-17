18c86b801b94c18fa76805cbb09dd2116acf9f68
Patrick Armstrong <parmstrong@scottlogic.co.uk>
2017-05-17 13:41:34 +0100

    Adds photo gallery of supplied images
    Using react-photo-gallery (https://github.com/neptunian/react-photo-gallery).
    
    No lightbox support yet.

--- jbc-app/src/App.js
+++ jbc-app/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
+import Gallery from 'react-photo-gallery';
 import Contact from './components/contact';
+import photos from './resources/photos';
 import './App.css';
 
 const App = () => (
+  <div>
   <Grid>
     <Row>
       <Col md={12}>
@@ -13,7 +16,13 @@ const App = () => (
           phone="0771 862 6084" />
       </Col>      
     </Row>
+    <Row>
+      <Col md={12}>
+        <Gallery photos={photos} />
+      </Col>
+    </Row>
   </Grid>
+  </div>
 );
 
 export default App;
